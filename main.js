e2705d21dd18db25d910df9beec347f614645b57
Ryan flanigan <flaners2@gmail.com>
2021-05-09 22:31:54 -0400

    Final project commit

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const childWindow = new BrowserWindow({
 })
   // and load the index.html of the app.
   mainWindow.loadFile('login.html')
-  childWindow.loadFile("addAssignment.html")
+
 
 
   // Open the DevTools.
@@ -38,7 +38,7 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createStartWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
